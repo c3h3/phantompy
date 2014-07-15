bebf721ea3cb37046d79cb9009d56d88a75796bb
Chia-Chi Chang <c3h3.tw@gmail.com>
2014-07-15 16:04:58 +0800

    create two helper functions

--- phantompy/settings.py
+++ phantompy/settings.py
@@ -12,3 +12,10 @@ TEMP_HTML_RES_DIR = os.path.join(PACKAGE_PATH, "TempResHTML")
 
 if not("TempResHTML" in os.listdir(PACKAGE_PATH)):
     os.mkdir(TEMP_HTML_RES_DIR)
+    
+def list_all_temp_files():
+    return os.listdir(TEMP_HTML_RES_DIR)
+
+def clean_all_temp_files():
+    map(lambda xx:os.remove(os.path.join(TEMP_HTML_RES_DIR,xx)),
+        list_all_temp_files())
